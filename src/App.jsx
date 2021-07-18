cfb46511b8278d33e3b08c5f9baf805a07a25ad7
Monika <mswidzinska87@gmail.com>
2021-07-18 23:44:14 +0200

    Load more functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-const url = "https://swapi.dev/api/people";
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [allPeople, setAllPeople] = useState([]);
+  const [page, setPage] = useState(2);
   const [error, setError] = useState("");
 
   useEffect(() => {
     setIsLoading(true);
-    fetch(url)
+    fetch("https://swapi.dev/api/people")
       .then((response) => {
         return response.json();
       })
@@ -22,6 +22,27 @@ function App() {
         setIsLoading(false);
       });
   }, []);
+
+  const getPeoplePerPage = () => {
+    setIsLoading(true);
+    fetch(`https://swapi.dev/api/people/?page=${page}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setAllPeople((old) => [...old, ...data.results]);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoading(false);
+      });
+  };
+
+  const loadMore = () => {
+    setPage(page + 1);
+    getPeoplePerPage();
+  };
   return (
     <div className="App">
       {isLoading ? (
@@ -31,6 +52,7 @@ function App() {
           {allPeople.map((item) => (
             <div key={item.name}>{item.name}</div>
           ))}
+          <button onClick={loadMore}>Load More...</button>
         </div>
       )}
       {error ? <p>{error}</p> : ""}
